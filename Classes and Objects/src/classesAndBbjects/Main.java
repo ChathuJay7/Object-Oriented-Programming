e1b22f68a4d096ac53bc2f3d236ac1942948c0b2
Jeewantha <chathurajmhb7@gmail.com>
2023-01-12 14:01:19 +0530

    Initializing

---
+++ Classes and Objects/src/classesAndBbjects/Main.java
@@ -0,0 +1,11 @@
+
+package classesAndBbjects;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.calArea();
+    }
+    
+}
---
+++ Classes and Objects/src/classesAndBbjects/Rectangle.java
@@ -0,0 +1,11 @@
+
+package classesAndBbjects;
+
+public class Rectangle {
+    int length = 5;
+    int width = 4;
+    
+    public void calArea(){
+        System.out.println("Area of rectangle = " + length*width);
+    }
+}
